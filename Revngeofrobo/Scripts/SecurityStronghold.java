9456633895046d09626d0a5721348216ae30b167
Revngeofrobo <revngeofrobo@gmail.com>
2018-11-04 12:59:25 -0500

    Update Revngeofrobo/Scripts/SecurityStronghold.java

--- Revngeofrobo/Scripts/SecurityStronghold.java
+++ Revngeofrobo/Scripts/SecurityStronghold.java
@@ -1,4 +1,4 @@
-package SecurityStrongholdFighter;
+package SecurityStronghold;
 import org.powerbot.script.*;
 import org.powerbot.script.rt4.*;
 import org.powerbot.script.rt4.ClientContext;
@@ -10,7 +10,7 @@ import java.awt.*;
 
 @Script.Manifest(name = "Security Stronghold Fighter", description = "Kills Flesh Crawlers, Giant Spiders, Minotaurs, or Zombies at the Security Stronghold", properties="Author = Revngeofstr")
 
-public class SecurityStrongholdFighter extends PollingScript<ClientContext> implements PaintListener {
+public class SecurityStronghold extends PollingScript<ClientContext> implements PaintListener {
 
     private String Food = "Tuna";
     private String MonsterToFight = "Zombie";
@@ -24,6 +24,16 @@ public class SecurityStrongholdFighter extends PollingScript<ClientContext> impl
     private final static int AirRune = 556;
 
 
+
+    //PathStuff
+    private final static int Entrance = 20790;
+    private final static int Portal = 20786; //Right after Entrance (not needed for mintars
+    private final static int Portal2 = 19005; //For Spiders
+    private final static int Ladder1 = 20785; //After portal 1
+    private final static int Ladder2 = 19004; //For Spiders
+    private boolean AtStronghold = false;
+
+    /* Old Stuck Tile Stuff
     private static final Tile StuckTile1 = new Tile(2027, 5239, 0);
     private static final Tile StuckTile2 = new Tile(2026, 5239, 0);
     private static final Tile StuckTile3 = new Tile(1976, 5184, 0);
@@ -40,54 +50,120 @@ public class SecurityStrongholdFighter extends PollingScript<ClientContext> impl
     private static final Tile StuckTile14 = new Tile (2132, 5279, 0);
     private static final Tile StuckTile15 = new Tile(2032, 5227, 0);
     private static final Tile StuckTile16 = new Tile (2031, 5227, 0);
+    */
+
+    public static final Tile[] PathToBank = {new Tile(3214, 3425, 0), new Tile(3211, 3425, 0),
+            new Tile(3208, 3426, 0), new Tile(3205, 3428, 0),
+            new Tile(3202, 3428, 0), new Tile(3199, 3429, 0),
+            new Tile(3196, 3429, 0), new Tile(3193, 3429, 0),
+            new Tile(3190, 3430, 0), new Tile(3187, 3430, 0),
+            new Tile(3184, 3431, 0), new Tile(3183, 3434, 0),
+            new Tile(3183, 3437, 0)};
 
 
-    public void Heal() {
+
+    public static final Tile[] PathToEntrance = {new Tile(3185, 3438, 0), new Tile(3183, 3435, 0),
+            new Tile(3182, 3432, 0), new Tile(3179, 3432, 0), new Tile(3176, 3431, 0), new Tile(3173, 3429, 0),
+            new Tile(3170, 3429, 0), new Tile(3167, 3428, 0), new Tile(3164, 3426, 0), new Tile(3161, 3424, 0),
+            new Tile(3158, 3423, 0), new Tile(3155, 3420, 0), new Tile(3152, 3417, 0), new Tile(3149, 3417, 0),
+            new Tile(3146, 3417, 0), new Tile(3143, 3417, 0), new Tile(3140, 3417, 0), new Tile(3137, 3417, 0),
+            new Tile(3134, 3417, 0), new Tile(3131, 3417, 0), new Tile(3128, 3416, 0), new Tile(3125, 3416, 0),
+            new Tile(3122, 3418, 0), new Tile(3119, 3418, 0), new Tile(3116, 3419, 0), new Tile(3113, 3419, 0),
+            new Tile(3110, 3419, 0), new Tile(3107, 3420, 0), new Tile(3104, 3420, 0), new Tile(3101, 3420, 0),
+            new Tile(3098, 3420, 0), new Tile(3095, 3420, 0), new Tile(3092, 3420, 0), new Tile(3089, 3420, 0),
+            new Tile(3086, 3420, 0), new Tile(3083, 3422, 0), new Tile(3080, 3423, 0)};
+
+    public static final Tile[] PathToLadder1 = {new Tile(1914, 5222, 0), new Tile(1911, 5222, 0),
+            new Tile(1908, 5221, 0), new Tile(1905, 5221, 0)};
+
+    public static final Tile[] PathToDoor1Flesh = {new Tile(2044, 5236, 0), new Tile(2041, 5235, 0),
+            new Tile(2044, 5234, 0), new Tile(2046, 5231, 0), new Tile(2043, 5230, 0),
+            new Tile(2043, 5227, 0), new Tile(2045, 5224, 0), new Tile(2045, 5221, 0),
+            new Tile(2044, 5218, 0), new Tile(2043, 5215, 0), new Tile(2040, 5213, 0),
+            new Tile(2038, 5210, 0), new Tile(2038, 5207, 0), new Tile(2037, 5204, 0)};
+
+    public static final Tile[] PathToDoor2Flesh = {new Tile(2037, 5200, 0), new Tile(2040, 5199, 0),
+            new Tile(2042, 5202, 0), new Tile(2045, 5202, 0), new Tile(2045, 5199, 0)};
+
+    public static final Tile[] PathToZombies = {new Tile(2036, 5244, 0), new Tile(2033, 5243, 0),
+            new Tile(2033, 5240, 0), new Tile(2031, 5237, 0), new Tile(2031, 5234, 0)};
+
+    public static final Tile[] PathToSpidersDoor1 = {new Tile(2123, 5252, 0), new Tile(2126, 5252, 0),
+            new Tile(2129, 5253, 0), new Tile(2132, 5255, 0)};
+
+    public static final Tile[] PathToSpiders = {new Tile(2133, 5260, 0), new Tile(2133, 5263, 0),
+            new Tile(2131, 5266, 0), new Tile(2129, 5269, 0)};
+
+    public static final Tile LadderTile = new Tile(1859, 5243, 0);
+
+
+
+    private void Heal() {
         Item FoodID = ctx.inventory.select().name(Food).poll();
         FoodID.interact("Eat");
         Condition.sleep(1000);
     }
 
-    public boolean ShouldAttack() {
-        return (!ctx.players.local().interacting().valid() &&  !Stuck());
+    private boolean InFightZone(){
+        return (ctx.players.local().contains()); //Add Boounds here based on monster
     }
 
-    public boolean HasFood() {
+    private boolean InBank(){
+        return (ctx.players.local().contains()); //Add Boounds here for the bank
+    }
 
+    private boolean ShouldAttack() {
+        return (!ctx.players.local().interacting().valid() && InFightZone() && HasFood());
+    } //Add if in region Fight to ShouldAttack for each monster
+
+    private boolean HasFood() {
         return (ctx.inventory.select().name(Food).count() > 0);
     }
 
-    public boolean NeedFood() {
-
+    private boolean NeedFood() {
         return (ctx.combat.health() < HealthToEat);
     }
 
-    public boolean CanCast() {
+    private boolean CanCast() {
         return (ctx.skills.level(Constants.SKILLS_MAGIC) > 24 && ctx.inventory.select().id(LawRune).count() > 0 && ctx.inventory.select().id(FireRune).count() > 0 && ctx.inventory.select().id(AirRune).count() > 2);
     }
-    public boolean HasTab() {
-
+    private boolean HasTab() {
         return (ctx.inventory.select().id(Tab).count() > 0);
     }
 
-    public void Cast() {
+    private void Cast() {
         ctx.magic.cast(Magic.Spell.VARROCK_TELEPORT);
-        Condition.sleep(10000);
-        Logout();
+        Condition.sleep(3000);
     }
-    public void UseTab() {
+    private void UseTab() {
         ctx.inventory.select().id(Tab).action("Use");
-        Condition.sleep(10000);
-        Logout();
+        Condition.sleep(3000);
     }
 
-    public void Leave() {
+    private boolean ShouldLeave(){
+        return  (!HasFood() && InFightZone());
+    }
+
+    private boolean NeedBank() {
+        return (!HasFood() && !InFightZone());
+    }
+
+
+    private boolean BackToFightZone() {
+        return (HasFood() && !InFightZone());
+    }
+
+
+
+
+    private void Leave() {
         if (CanCast()) {
             Cast();
         }else if (HasTab()){
             UseTab();
         }else{
-            OpenDoorToLeave();
+            OpenDoor();
+            Logout();
         }
 
     }
@@ -116,23 +192,22 @@ public class SecurityStrongholdFighter extends PollingScript<ClientContext> impl
         }
     }
 
-    public void OpenDoorToLeave() {
-        final GameObject DoorToLeave = ctx.objects.select().id(Door).nearest().poll();
+    public void OpenDoor() {
+        final GameObject DoorToOpen = ctx.objects.select().id(Door).nearest().poll();
 
-        if (DoorToLeave.inViewport()) {
-            DoorToLeave.interact("Open");
-            Condition.sleep(3000);
-            Logout();
-        }else{
-            ctx.camera.turnTo(DoorToLeave);
+        if (DoorToOpen.inViewport()) {
+            DoorToOpen.interact("Open");
             Condition.sleep(800);
-            DoorToLeave.interact("Open");
-            Condition.sleep(3000);
-            Logout();
+        }else{
+            ctx.camera.turnTo(DoorToOpen);
+            Condition.sleep(800);
+            DoorToOpen.interact("Open");
+            Condition.sleep(800);
         }
     }
 
-    //Temp fix to the mis-click the door problem
+
+    /*Stuck Tile problem fixed by walking
     public boolean Stuck(){
         return (ctx.players.local().tile().equals(StuckTile1) || ctx.players.local().tile().equals(StuckTile2) || ctx.players.local().tile().equals(StuckTile3)
                 || ctx.players.local().tile().equals(StuckTile4) || ctx.players.local().tile().equals(StuckTile5) || ctx.players.local().tile().equals(StuckTile6)
@@ -141,6 +216,7 @@ public class SecurityStrongholdFighter extends PollingScript<ClientContext> impl
                 || ctx.players.local().tile().equals(StuckTile13) || ctx.players.local().tile().equals(StuckTile14)) || ctx.players.local().tile().equals(StuckTile15)
                 || ctx.players.local().tile().equals(StuckTile16);
     }
+    */
 
     public void Logout() {
         ctx.game.logout();
@@ -149,11 +225,6 @@ public class SecurityStrongholdFighter extends PollingScript<ClientContext> impl
             ctx.controller.stop();
         }
     }
-
-    //Impliment method for if stuck to re-open door
-    //Walking from bank to fight zone
-
-
 
 
     @Override
@@ -196,20 +267,68 @@ public class SecurityStrongholdFighter extends PollingScript<ClientContext> impl
     }
 
 
+    private State state() {
+
+        if (NeedFood())
+            return State.Eat;
+
+        if(ShouldAttack())
+            return State.Attack;
+
+        if (ShouldLeave())
+            return State.Leave;
+
+        if (NeedBank() && !InBank())
+            return State.WalkToBank;
+
+        if (NeedBank() && InBank())
+            return State.Bank;
+
+        if(BackToFightZone())
+            return State.WalkToMonster;
+    }
+
+
+
 
     @Override
     public void poll() {
-        if (HasFood()){
-            if (NeedFood()) {
+
+        final State state = state();
+
+        // now we want a switch statement checking our current state
+        switch (state) {
+
+            // now we make a case for each state we have
+            case Eat:
                 Heal();
-            } else if (ShouldAttack()) {
+                break;
+            case Attack:
                 Attack();
-            }else if (Stuck()){
-                Logout();
-            }
-        }else{
-            Leave();
+                break;
+            case Leave:
+                Leave();
+                break;
+            case Bank:
+
+                break;
+            case WalkToBank:
+
+                break;
+            case WalkToMonster:
+
+                break;
+
+                default:
+                break;
         }
+    }
+
+
+
+
+    private enum State {
+        WalkToBank, Bank, WalkToMonster, Eat, Attack, Leave
     }
 
     @Override
